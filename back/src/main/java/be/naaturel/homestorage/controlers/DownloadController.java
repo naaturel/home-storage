0abe105134a059e7b8a15d2a166eb09efad8ff0d
Laurent <58115082+naaturel@users.noreply.github.com>
2024-09-25 13:49:18 +0200

    feature(file-storage) : File download is up

--- back/src/main/java/be/naaturel/homestorage/controlers/DownloadController.java
+++ back/src/main/java/be/naaturel/homestorage/controlers/DownloadController.java
@@ -4,11 +4,21 @@ import be.naaturel.homestorage.configurations.Configurations;
 import be.naaturel.homestorage.repository.DocumentRepo;
 import be.naaturel.homestorage.services.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Set;
 
 @RestController
@@ -29,6 +39,25 @@ public class DownloadController {
 
         Set<String> files = fileService.list(conf.storageLocation);
         return ResponseEntity.ok(files);
+    }
+
+    @GetMapping("api/download/{fileName:.+}")
+    public ResponseEntity<?> download(@PathVariable String fileName){
+
+        Path path = Paths.get(conf.storageLocation + fileName);
+        Resource resource = null;
+
+        try {
+            resource = new UrlResource(path.toUri());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return new ResponseEntity<Error>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("multipart/form-data"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
 }
