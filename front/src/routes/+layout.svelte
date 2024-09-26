7996d370ce0d95d689e5f9abea7f86b018df5c41
Laurent <58115082+naaturel@users.noreply.github.com>
2024-09-26 12:16:13 +0200

    feature(front-login-page) : style of Login page almost finished

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,5 +1,34 @@
-<nav>
-    <a href="/">Home</a>
+<script>
+    import Button from "../components/Button.svelte";
+</script>
+
+<nav class="bordered">
+
+    <Button link="/"> Home </Button>
+    <Button link="/login"> Login </Button>
+    <Button link="/logout"> Logout </Button>
+    <Button link="/download"> Download </Button>
+    <Button link="/upload"> Upload </Button>
+
 </nav>
 
-<slot/>
+<hr/>
+
+<div class="container">
+    <slot/>
+</div>
+
+
+<hr/>
+
+<footer>
+    <a href="https://github.com/naaturel">Github</a>
+    <a href="https://twitter.com/naaturel_">Twitter</a>
+</footer>
+
+<!--
+Primary : #F5F5F5
+Secondary : #B9E937
+Tertiary : #00B906
+Secondary : #424242
+-->
--- front/src/routes/login/+page.svelte
+++ front/src/routes/login/+page.svelte
@@ -1,27 +1,59 @@
 <script>
-    import { onDestroy } from 'svelte';
-    //import TopNavBar from './TopNavBar.svelte';
-    //import { User } from './store.js';
 
-    let user;
-    //const unUser = User.subscribe(v => user = v);
-    //onDestroy(unUser);
+import Button from "../../components/Button.svelte";
 
-    $: isLoggedIn = !!user;
+const buttonStyle = {
+    "box-shadow" : "none",
+    "text-decoration" : "underline",
+    "margin-top" : "1vh",
+}
+
 </script>
 
-{#if isLoggedIn}
+    <h1>Log in to start</h1>
 
-    <p>
-        if you click "log out", you should see only "log in" button.
-    </p>
-    <p>
-        To see the bug, navigate to "TopNavBar.svelte" tab and uncomment <br>
-        //const unUser = User.subscribe(v => user = v);
-    </p>
-{:else}
-    <button on:click>log in</button>
-    <p>
-        click "login"
-    </p>
-{/if}
+    <form>
+
+        <input class="bordered" placeholder="Username">
+        <input class="bordered" placeholder="Password">
+
+        <Button {buttonStyle}> Log me in ! </Button>
+        <a href="">Not registered yet ? Let's get started</a>
+    </form>
+
+
+<style>
+
+
+    form
+    {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        margin: 10vh;
+    }
+
+    input
+    {
+        color: #424242;
+        font-size: 2.5vh;
+
+        width: 20vw;
+        height: 4.5vh;
+
+        padding: 1vh;
+        margin: 2.5vh;
+
+        text-align: center;
+    }
+
+    a
+    {
+        margin-bottom: 3vh;
+        margin-top: 3vh;
+        font-size: 1.25vh;
+        border-bottom: solid 1px #F5F5F5;
+    }
+
+</style>
