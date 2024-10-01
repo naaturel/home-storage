8a10dcd03006d6b75563f8f7d0233c93ffe1e1c9
Laurent <58115082+naaturel@users.noreply.github.com>
2024-10-01 15:39:55 +0200

    Fixe user authentication

--- back/src/main/java/be/naaturel/homestorage/configurations/Security.java
+++ back/src/main/java/be/naaturel/homestorage/configurations/Security.java
@@ -1,25 +1,18 @@
 package be.naaturel.homestorage.configurations;
 
-import be.naaturel.homestorage.services.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -39,8 +32,8 @@ public class Security {
                 .cors(cors -> {})
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/login", "/error").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/**" ).permitAll()
+                        //.anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
                         .defaultSuccessUrl("/", true)
---
+++ back/src/main/java/be/naaturel/homestorage/exceptions/AuthenticationException.java
@@ -0,0 +1,9 @@
+package be.naaturel.homestorage.exceptions;
+
+public class AuthenticationException extends Exception{
+
+    public AuthenticationException(String message){
+        super(message);
+    }
+
+}
